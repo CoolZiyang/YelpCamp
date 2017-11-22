f0cbd3e2e1bf8636f6a1b7eb2bbf1704becfbec5
Ziyang Wang <ziyangwang2016@gmail.com>
2017-11-22 21:00:49 +0000

    add user authentication to the route when the user wants to create a new campground or add a new comment

--- app.js
+++ app.js
@@ -1,94 +1,43 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User        = require("./models/user"),
+    SeedDB      = require("./seeds");
+
+// requiring routes
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+SeedDB(); //seed the datebase
 
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
 });
 
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "John Rock",
-//         image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/Galleries/North+Carolina+Hikes/Slide1.jpg",
-//         description: "This hike takes you past a small but attractive waterfall, up to the top of John Rock, which is the large mountain you see looming behind the Fish Hatchery parking area in Pisgah National Forest. Views of the valley below, up to the Pisgah Ridge, and across to Looking Glass Rock are this hike's main scenic attraction besides the waterfall. It also passes by tranquil meadows near Picklesimer Fields, and through some nice displays of wildflowers in the spring."
-        
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             console.log("NEWLY CREATED CAMPGROUND: ");
-//             console.log(campground);
-//         }
-//     });
-
-// var campgrounds = [
-//         {name: "Cedar Bluff State Park", image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/Galleries/The+Best+Fishing+Holes/Glen-Elder-580x370.jpg"},
-//         {name: "John Rock", image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/Galleries/North+Carolina+Hikes/Slide1.jpg"},
-//         {name: "The Ice Age Trail (Wisconsin)", image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/580/Ice+Age+Trail.jpg"},
-//         {name: "The Hayduke Trail (Utah to Arizona)", image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/580/The+Hayduke+Trail.jpg"},
-//         {name: "Schodack Island State Park", image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/580/NY+State+Parks+for+Fall/Schodack+Island+State+Park.jpg"},
-//         {name: "Guajome Regional Park", image: "http://www.sdparks.org/content/dam/sdparks/en/images/park-images/Guajome_3.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg"},
-//         {name: "Cheney State Park", image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/Galleries/The+Best+Fishing+Holes/Cheney-580x370.jpg"}
-//     ];
-
-app.get("/", function(req, res){
-    res.render("landing");
-});
-
-// Index - show all of campgrounds
-app.get("/campgrounds", function(req, res){
-    // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("index", {allCampgrounds:allCampgrounds});
-        }
-    });
-});
-
-// Create - add the new campground to database
-app.post("/campgrounds", function(req, res){
-    //get data from posts and add it to campgrounds array
-    var name=req.body.name;
-    var image=req.body.image;
-    var desc=req.body.description;
-    var newCampground={name : name, image : image, description : desc}; 
-    //create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log(err);    
-        } else {
-            res.redirect("/campgrounds");
-        }
-    });
-});
-
-// New - show form to create a new campground
-app.get("/campgrounds/new", function(req, res){
-    res.render("new"); 
-});
-
-// SHOW - shows more info about one campground
-app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("show", {foundCampground: foundCampground});
-        }
-    });
-});
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
---
+++ seeds.js
@@ -0,0 +1,57 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Cedar Bluff State Park",
+        image: "http://ksoutdoors.com/var/news/storage/images/state-parks/locations/cedar-bluff/cedar-bluff-gallery/cedar-bluff-cliff-view/453013-4-eng-US/Cedar-Bluff-Cliff-View_gallery.png",
+        description: "Cedar Bluff State Park is divided into two, unique areas along the shorelines of Cedar Bluff Reservoir. The Bluffton Area, on the north shore, provides nearly 350 acres for visitors."
+    },
+    {
+        name: "Cheney State Park",
+        image: "http://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Outdoors/Galleries/The+Best+Fishing+Holes/Cheney-580x370.jpg",
+        description: "Cheney State Park is a state park of Kansas in the United States. Completed in 1964, the park is located in Kingman and Reno counties in Kansas, 5 miles north of Cheney and 20 miles west of Wichita."
+    },
+    {
+        name: "The Hayduke Trail (Utah to Arizona)",
+        image: "https://andrewskurka.korndev-cdn.com/wp-content/uploads/featured_hayduke.jpg",
+        description: "The Hayduke Trail is an 812-mile (1,307 km) backpacking route across southern Utah and northern Arizona.It begins in Arches National Park near Moab, Utah, before heading through the Needles district of Canyonlands National Park, Capitol Reef National Park, Bryce Canyon National Park, the Grand Canyon National Park and ending in Zion National Park.",
+    }
+];
+
+function seedDB(){
+    //remove all campgrounds
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        //add seeded campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log("added a campground");
+                    //add seeded comments
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was Internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new commet");
+                            }
+                    });
+                }
+            });
+        });
+    });
+}
+
+module.exports = seedDB;
