6bd2b50d45cb9659631b295c92a8a0b800b80cbd
Ziyang Wang <ziyangwang2016@gmail.com>
2017-11-28 23:54:42 +0000

    add head alert and price feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 //SeedDB(); //seed the datebase
 
 // PASSPORT CONFIGURATION
@@ -34,6 +36,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
--- middleware/index.js
+++ middleware/index.js
@@ -7,16 +7,23 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
         if(err){
-            res.redirect("/campgrounds");
+            req.flash("error", "Campground not found");
+            res.redirect("back");
         } else{
+        if (!foundCampground) {
+                req.flash("error", "Item not found.");
+                return res.redirect("back");
+            }
         if(foundCampground.author.id.equals(req.user._id)){
             next();
         } else{
-            res.send("YOU DO NOT HAVE PERMISSION!");
+            req.flash("error", "You don't have permission to do that");
+            res.redirect("back");
         }
         }
         });
     } else{
+        req.flash("error", "You need to log in first");
         res.redirect("back");
     }
 };
@@ -25,16 +32,23 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
+            req.flash("error", "Comment not found");
             res.redirect("back");
         } else{
+        if (!foundComment) {
+                req.flash("error", "Item not found.");
+                return res.redirect("back");
+            }
         if(foundComment.author.id.equals(req.user._id)){
             next();
         } else{
+            req.flash("error", "You don't have permission to do that");
             res.redirect("back");
         }
         }
         });
     } else{
+        req.flash("error", "You need to log in first");
         res.redirect("back");
     }
 };
@@ -43,6 +57,7 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
+    req.flash("error", "You need to log in first");
     res.redirect("/login");
 };
 
